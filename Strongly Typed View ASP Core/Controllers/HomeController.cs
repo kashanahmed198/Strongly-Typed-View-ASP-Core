ab63ec7aa7366be6d037c74c41ce3ccfa18e40ca
kashanahmed198 <eng.kashanahmed198@gmail.com>
2024-11-12 20:23:23 +0500

    Add project files.

---
+++ Strongly Typed View ASP Core/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Strongly_Typed_View_ASP_Core.Models;
+using System.Diagnostics;
+
+namespace Strongly_Typed_View_ASP_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //Employee obj = new Employee()
+            //{
+            //    EmpID = 101,
+            //    EmpName = "Affan",
+            //    Designation = "Manaaer",
+            //    Salary = 55000,
+            //};
+
+            var myEmployees = new List<Employee>
+            {
+                new Employee { EmpId = 101, EmpName = "Kashan", Designation = "Developer", Salary = 50000},
+                new Employee { EmpId = 102, EmpName = "Ahmed", Designation = "Developer", Salary = 40000},
+                new Employee { EmpId = 103, EmpName = "Ahtasham", Designation = "Developer", Salary = 55000},
+                new Employee { EmpId = 104, EmpName = "Umair", Designation = "Developer", Salary = 35000},
+                new Employee { EmpId = 105, EmpName = "Noman", Designation = "Developer", Salary = 45000}
+            };
+            return View(myEmployees);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Strongly Typed View ASP Core/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿ namespace Strongly_Typed_View_ASP_Core.Models
+{
+    public class Employee
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public string Designation { get; set; }
+        public int Salary { get; set; }
+    }
+}
